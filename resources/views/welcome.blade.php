53b6127cedc964ee7cd19e182521dd0031bb59c7
ThePope05 <swobben05@gmail.com>
2024-02-16 18:22:12 +0100

    Updated navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,6 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        </h2>
-    </x-slot>
-
     <div class="py-12 bg-slate-800 flex justify-center">
-        <h1 class="text-4xl text-white">Welcome to your profile</h1>
+        <h1 class="text-8xl text-bold text-white pt-60 ">WELCOME TO RACE TIMES</h1>
+        <hr>
     </div>
 </x-app-layout>
