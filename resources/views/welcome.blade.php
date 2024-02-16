2e1d14328d4b3b3522fd8d540bdce211feef95df
ThePope05 <swobben05@gmail.com>
2024-02-16 18:43:55 +0100

    Updated all front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="py-12 bg-slate-800 flex justify-center">
-        <h1 class="text-8xl text-bold text-white pt-60 ">WELCOME TO RACE TIMES</h1>
+    <div class="py-12 bg-m3 bg-center bg-no-repeat bg-cover flex justify-center">
+        <h1 class="text-8xl font-black text-white pt-60 ">WELCOME TO RACE TIMES</h1>
         <hr>
     </div>
 </x-app-layout>
