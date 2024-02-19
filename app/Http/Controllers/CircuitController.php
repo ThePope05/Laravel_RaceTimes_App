8ac78cd985237648a7f53e098cfc345ddbea6488
ThePope05 <swobben05@gmail.com>
2024-02-19 14:15:23 +0100

    Updated migrations

---
+++ app/Http/Controllers/CircuitController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Circuit;
+
+class CircuitController extends Controller
+{
+    public function index()
+    {
+        $circuits = Circuit::all();
+        return view('circuit.index', compact('circuits'));
+    }
+
+    public function create()
+    {
+        return view('circuit.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'country' => 'required',
+            'length' => 'required',
+        ]);
+
+        $circuit = Circuit::create($request->all());
+
+        return redirect()->route('circuit.show', $circuit);
+    }
+
+    public function show(Circuit $circuit)
+    {
+        return view('circuit.show', compact('circuit'));
+    }
+
+    public function edit(Circuit $circuit)
+    {
+        return view('circuit.edit', compact('circuit'));
+    }
+
+    public function update(Request $request, Circuit $circuit)
+    {
+        $request->validate([
+            'name' => 'required',
+            'country' => 'required',
+            'length' => 'required',
+        ]);
+
+        $circuit->update($request->all());
+
+        return redirect()->route('circuit.show', $circuit);
+    }
+
+    public function destroy(Circuit $circuit)
+    {
+        $circuit->delete();
+
+        return redirect()->route('circuit.index');
+    }
+}
