cd1b290dc806e549985544781994e8d060579b72
ThePope05 <swobben05@gmail.com>
2024-02-19 10:31:00 +0100

    Added relation to circuit and time

--- app/Models/Circuit.php
+++ app/Models/Circuit.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Circuit extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'country',
+        'length',
+    ];
+
+    public function times()
+    {
+        return $this->hasMany(Time::class);
+    }
 }
