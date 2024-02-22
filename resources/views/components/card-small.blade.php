06b9aea83282497d71ad62719b4636b996dc3999
ThePope05 <swobben05@gmail.com>
2024-02-22 14:05:05 +0100

    Refactor card component styles

--- resources/views/components/card-small.blade.php
+++ resources/views/components/card-small.blade.php
@@ -1,3 +1,12 @@
-<div class="p-4 m-4 mb-16 w-2/5 min-h-48 h-max flex flex-col justify-center align-middle bg-gradient-to-tr from-slate-700 to-slate-900 rounded-lg hover:scale-105 transition-transform">
+@php
+$class = 'p-4 m-4 mb-16 w-2/5 min-h-48 h-max flex flex-col justify-center align-middle bg-gradient-to-tr from-slate-700 to-slate-900 rounded-lg hover:scale-105 transition-transform ';
+if (isset($attributes['class'])){
+$attributes['class'] = $class . $attributes['class'];
+} else {
+$attributes['class'] = $class;
+}
+@endphp
+
+<div class="{{ $attributes['class'] }}">
     {{ $slot }}
 </div>
