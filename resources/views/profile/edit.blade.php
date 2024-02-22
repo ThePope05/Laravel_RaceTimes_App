444f9ba9a3ceef81c05a147f5468aa0795e8223b
ThePope05 <swobben05@gmail.com>
2024-02-22 13:56:49 +0100

    Refactor profile edit page layout

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,25 +3,14 @@
         {{ strtoupper(Auth::user()->name) }}
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+    <x-card-small>
+        @include('profile.partials.update-password-form')
+    </x-card-small>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+    <x-card-small>
+        @include('profile.partials.update-profile-information-form')
+        <hr class="my-4">
+        @include('profile.partials.delete-user-form')
+    </x-card-small>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
